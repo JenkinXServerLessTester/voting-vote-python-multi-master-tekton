43ca4458a87d78d28a83511349c6195516f036dd
q13117god <carlxie2015@gmail.com>
2019-07-23 00:39:26 +0000

    After attach AWSECRpolicy

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "A Calm Cloud Cats")
-option_b = os.getenv('OPTION_B', "A Calm Cloud Dogs")
+option_b = os.getenv('OPTION_B', "A little Calm Cloud Dogs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
